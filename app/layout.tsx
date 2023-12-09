c786c61709607335c33a88fc09a0b58960cc74f0
Eliana Marquez <eliana.marquez.ola@gmail.com>
2023-12-09 11:10:09 -0500

    initial state

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import './ui/global.css';
+import { inter } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items center'> Hecho con Amor!</footer>
+      </body>
+    </html>
+  );
+}
